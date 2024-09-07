fd9bf4c5b643d6e226c72d2eb0b1297bb163fc3a
Eunixia <eunixiayuson@outlook.com>
2024-09-07 17:13:24 +0800

    multiplication.c
    Take 2

--- c/codes/multiplication.c
+++ c/codes/multiplication.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-
 int main() {
     int num1, num2, result;
 
-    printf("Enter two integers: ");
-    scanf("%d %d", &num1, &num2);
+    printf("Basic Multuplication"); 
+
+    printf("Enter the first interger: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second interger: ");
+    scanf("%d", &num2);
 
     result = num1 * num2;
 
