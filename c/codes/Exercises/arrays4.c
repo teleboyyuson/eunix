2a1f5087addf852e39c15aaebbf9893312b18096
Eunixia <eunixiayuson@outlook.com>
2024-12-09 18:39:32 +0800

    3rd commit

---
+++ c/codes/Exercises/arrays4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   // array = a data structure that can store many values of the same data type.
+    double prices[10] = {5.0, 10.0, 15.0, 25.0, 20.0};
+
+    printf("$%.21f/n", prices[0]);
+
+    return 0;
+}
