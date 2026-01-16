e741f64f485c4c3e96f435530123e2e2bb815f1e
Eunixia <eunixiayuson@outlook.com>
2026-01-16 08:32:25 +0800

    Add files via upload
    Concept: Variables, strings, chars, bool, assignments

---
+++ c & c++/id.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "Eunix";
+    int id = 10245;
+    char section = 'A';
+    bool active = true;
+
+    cout << "=== DIGITAL ID ===\n";
+    cout << "Name: " << name << endl;
+    cout << "ID: " << id << endl;
+    cout << "Section: " << section << endl;
+    cout << "Active: " << active << endl;
+    return 0;
+}
