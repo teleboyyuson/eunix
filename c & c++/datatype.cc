f617a4c01ddc5f98893865ed6cc874762e1edbc3
Eunixia <eunixiayuson@outlook.com>
2026-01-16 14:41:07 +0800

    Data Type Summary Printer
    Concept: Summary of variables & data types

---
+++ c & c++/datatype.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    float f = 2.5f;
+    double d = 3.14159;
+    char c = 'C';
+    bool b = false;
+
+    cout << "int: " << i << endl;
+    cout << "float: " << f << endl;
+    cout << "double: " << d << endl;
+    cout << "char: " << c << endl;
+    cout << "bool: " << b << endl;
+
+    return 0;
+}
