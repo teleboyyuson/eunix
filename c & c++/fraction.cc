c0474f0802df72cfe9721286032583286ef9894b
Eunixia <eunixiayuson@outlook.com>
2026-01-16 14:37:12 +0800

    Fraction test
    Concept: Float vs double

---
+++ c & c++/fraction.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float f = 1.1234567f;
+    double d = 1.1234567890123;
+
+    cout << "Float: " << f << endl;
+    cout << "Double: " << d << endl;
+
+    return 0;
+}
