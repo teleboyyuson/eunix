0cce51d4e831299965b9f4e76aebd7bd500c9041
Eunixia <eunixiayuson@outlook.com>
2026-01-16 14:46:57 +0800

    Character Encode Viewer
    Concept: char, int conversion

---
+++ c & c++/characencode.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letter = 'A';
+
+    cout << "Character: " << letter << endl;
+    cout << "ASCII Value: " << int(letter) << endl;
+
+    return 0;
+}
