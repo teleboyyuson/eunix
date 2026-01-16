ba808cdfbaabdd8b5127a81c9c3387ef7f580cae
Eunixia <eunixiayuson@outlook.com>
2026-01-16 14:39:15 +0800

    Boolean logic checker
    Concept: Booleans, comparisons

---
+++ c & c++/boolean.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score = 78;
+    bool passed = score >= 75;
+
+    cout << "Score: " << score << endl;
+    cout << "Passed: " << passed << endl;
+
+    return 0;
+}
