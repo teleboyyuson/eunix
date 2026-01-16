9a4e91c7126e0ac52f2a5ba2213b8ca55eeefd01
Eunixia <eunixiayuson@outlook.com>
2026-01-16 08:33:13 +0800

    Number Converter

---
+++ c & c++/num_converter.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 25;
+
+    cout << "Decimal: " << num << endl;
+    cout << "Binary: " << 0b11001 << endl;
+    cout << "Octal: " << 031 << endl;
+    cout << "Hex: " << 0x19 << endl;
+
+    return 0;
+}
